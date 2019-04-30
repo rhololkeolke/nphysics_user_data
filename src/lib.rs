ec22507c96aa42ea6fe463934dc05986712ba52c
Devin Schwab <digidevin@gmail.com>
2019-04-30 19:37:01 -0400

    :sparkles: Added HasColor trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use nalgebra as na;
+
+pub trait HasColor {
+    fn color(&self) -> na::Point3<f32>;
 }
