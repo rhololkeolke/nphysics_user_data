a94553b56c3ee8ee3137fe85299471476bb0882c
Devin Schwab <digidevin@gmail.com>
2019-05-01 19:41:26 -0400

    :wastebasket: Removed user_data field from ColliderUserData

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use nalgebra as na;
-use std::any::Any;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ColliderUserData<N>
 where
     N: na::RealField,
@@ -9,7 +8,6 @@ where
     pub rgba: Option<na::Point4<f32>>,
     pub torsional_friction: N,
     pub rolling_friction: N,
-    pub user_data: Option<Box<Any>>,
 }
 
 impl<N> Default for ColliderUserData<N>
@@ -21,7 +19,6 @@ where
             rgba: None,
             torsional_friction: N::from(0.005),
             rolling_friction: N::from(0.0001),
-            user_data: None,
         }
     }
 }
