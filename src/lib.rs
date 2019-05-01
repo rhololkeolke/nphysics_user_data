981053b70d753adbfb5c33061391c2563bcf385f
Devin Schwab <digidevin@gmail.com>
2019-05-01 14:57:12 -0400

    :sparkles: Created a ColliderUserData struct
    :wastebasket: Removed HasColor trait
    
    Any can't downcast to a trait without going through the intermediate
    type. So I'm just moving the common bits of collider user data into a
    common dependency package. This way the testbed and model explorer
    doesn't need to explicitly rely on mjcf-parser or vice-versa

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 use nalgebra as na;
+use std::any::Any;
 
-pub trait HasColor {
-    fn color(&self) -> na::Point3<f32>;
+#[derive(Debug)]
+pub struct ColliderUserData<N>
+where
+    N: na::RealField,
+{
+    pub rgba: Option<na::Point4<f32>>,
+    pub torsional_friction: N,
+    pub rolling_friction: N,
+    pub user_data: Option<Box<Any>>,
+}
+
+impl<N> Default for ColliderUserData<N>
+where
+    N: na::RealField + From<f32>,
+{
+    fn default() -> Self {
+        ColliderUserData {
+            rgba: None,
+            torsional_friction: N::from(0.005),
+            rolling_friction: N::from(0.0001),
+            user_data: None,
+        }
+    }
 }
